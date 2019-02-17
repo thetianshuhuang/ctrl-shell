72f1480311080c9c6c048748b4da239ae88dd910
Tianshu Huang <thetianshuhuang@gmail.com>
2019-02-17 16:50:11 -0600

    Added .add, .remove, .cmd

--- shell.py
+++ shell.py
@@ -1,3 +1,5 @@
+"""
+"""
 
 import os
 import subprocess
@@ -7,6 +9,40 @@ from .view import get_return
 
 
 class ctrlshellCommand(sublime_plugin.WindowCommand):
+    """ctrlshell Plugin Main Command
+
+    Attributes
+    ----------
+    __PROJ_LABEL : str
+        Label for command output of list project folders (.proj, alias '...')
+    """
+
+    __PROJ_LABEL = '// Project Folders //'
+    __DOC = """
+    ___ _       _ ___ _        _ _
+   / __| |_ _ _| / __| |_  ___| | |
+  | (__|  _| '_| \__ \ ' \/ -_) | |
+   \___|\__|_| |_|___/_||_\___|_|_|
+               v0.1 | Tianshu Huang
+
+Sublime utility shell inspired by emacs' [ctrl+x, ctrl+f} behavior
+
+Commands
+--------
+<filepath>
+    Change directory and list files or open file and create if not present;
+    emulates emacs' [ctrl+x, ctrl+f] behavior
+.proj [aliases: ...]
+    List current project folders
+.add <filepath>
+    Add folder to current project
+.remove <basename>
+    Remove folder from project folders
+.about [aliases: .help, .info]
+    Show information
+.cmd [aliases: .bash] <command>
+    Run command
+"""
 
     def run(self):
         self.window.show_input_panel(
@@ -19,31 +55,142 @@ class ctrlshellCommand(sublime_plugin.WindowCommand):
         view_id = get_return("view_id")
         previous = get_return("label")
 
+        # Close current view if current view is a generated view
         if view_id != 0:
             self.window.run_command(
                 "viewmanager", {"method": "close_view", "label": ""})
 
-        if text == "...":
+        sp = text.split(" ")
+        base = sp[0]
+        arg = " ".join(sp[1:])
+
+        if base in ["...", ".proj"]:
             self.__show_proj()
+        elif base in [".about", ".info", ".help"]:
+            self.__show(self.__DOC)
+        elif base in [".add"]:
+            self.__add_folder(arg)
+        elif base in [".remove"]:
+            self.__remove(arg)
+        elif base in [".cmd", ".bash"]:
+            self.__cmd(arg)
         else:
-            targetpath = None
-            if previous == '// Project Folders //':
-                for folder in self.window.folders():
-                    if os.path.basename(folder) == text:
-                        targetpath = folder
-                        break
+            self.__file_explore(text, previous)
 
-            if targetpath is None:
-                targetpath = os.path.expanduser(text)
+    def __cmd(self, text):
+        """Execute shell command
 
-            try:
-                os.chdir(targetpath)
-                self.__show(self.__get_ls())
-            except (FileNotFoundError, NotADirectoryError):
-                self.__open_file(text)
+        Parameters
+        ----------
+        text : str
+            Command to run
+        """
+
+        self.__show(
+            "[shell][{cwd}]: {cmd}\n\n{res}".format(
+                cwd=os.getcwd(), cmd=text,
+                res=subprocess.check_output(text.split(" ")).decode("utf-8")))
+
+    def __remove(self, target):
+        """Remove folder from project
+
+        Parameters
+        ----------
+        target : str
+            Folder to remove
+        """
+
+        f = self.window.project_data()
+        f['folders'] = [
+            folder for folder in f['folders'] if
+            os.path.basename(folder['path']) != target]
+
+        self.window.set_project_data(f)
+
+    def __add_folder(self, target):
+        """Add folder to project
+
+        Parameters
+        ----------
+        target : str
+            Folder to add (if present)
+        """
+
+        current = os.getcwd()
+
+        try:
+            os.chdir(os.path.expanduser(target))
+
+            f = self.window.project_data()
+            add = {'follow_symlinks': True, 'path': os.getcwd()}
+            if not f:
+                f = {'folders': [add]}
+            else:
+                f['folders'].append(add)
+
+            self.window.set_project_data(f)
+
+        except FileNotFoundError:
+            print("Folder does not exist.")
+        except NotADirectoryError:
+            print("Target directory must be a folder.")
+        finally:
+            os.chdir(current)
+
+    def __file_explore(self, target, previous):
+        """Standard file-explorer command
+
+        Parameters
+        ----------
+        target : str
+            Target name
+        previous : str
+            Previous target
+        """
+
+        targetpath = self.__search_proj(target, previous)
+        try:
+            os.chdir(targetpath)
+            self.__show(self.__get_ls())
+        except (FileNotFoundError, NotADirectoryError):
+            self.window.open_file(os.path.join(os.getcwd(), targetpath))
+
+    def __search_proj(self, target, previous):
+        """Search for a target basename in current open folders if the
+        previous window was a project listing
+
+        Parameters
+        ----------
+        target : str
+            Target basename or filepath
+        previous : str
+            Previous name; triggers search if previous is project listing
+        """
+
+        # search in folders if previous is project folder listing
+        if previous == self.__PROJ_LABEL:
+            target = target.split(os.sep)[0]
+            for folder in self.window.folders():
+                if os.path.basename(folder) == target:
+                    return folder
+        # default - normal user input
+        return os.path.expanduser(target)
 
     def __get_ls(self, target=None):
+        """Get contents of target directory
 
+        Parameters
+        ----------
+        target : str or None
+            Target directory; if None, the current working dir is used.
+
+        Returns
+        -------
+        str
+            Result of 'ls -l -a' with added header
+        """
+
+        # Change directory
         if target is not None:
             current = os.getcwd()
             os.chdir(target)
@@ -51,26 +198,35 @@ class ctrlshellCommand(sublime_plugin.WindowCommand):
         fpath = os.path.basename(os.getcwd())
         ret = subprocess.check_output(["ls", "-l", "-a"]).decode("utf-8")
 
+        # Restore working dir
         if target is not None:
             os.chdir(current)
 
-        base = "{div}\n|   {fpath}   |  \n{div}\n".format(
-            fpath=fpath, div='-' * (len(fpath) + 8))
-
-        return "{base}[{fullpath}]\n\n{ret}".format(
+        return "{div}\n|   {fpath}   |\n{div}\n[{fullpath}]\n\n{ret}".format(
             ret=ret,
-            fullpath=os.getcwd(),
-            base=base,)
+            fullpath=target if target is not None else os.getcwd(),
+            fpath=fpath,
+            div='-' * (len(fpath) + 8))
 
     def __show_proj(self):
+        """Show project folder contents"""
 
         self.__show(
             "\n".join([
                 self.__get_ls(folder)
                 for folder in self.window.folders()]),
-            label="// Project Folders //")
+            label=self.__PROJ_LABEL)
 
     def __show(self, text, label=None):
+        """Show text in a new temp view
+
+        Parameters
+        ----------
+        text : str
+            Text to show
+        label : str or None
+            File label; if None, uses the current directory
+        """
 
         self.window.run_command(
             "viewmanager", {
@@ -80,8 +236,3 @@ class ctrlshellCommand(sublime_plugin.WindowCommand):
 
         self.window.active_view().run_command(
             "inserttext", {"text": text, "point": 0})
-
-    def __open_file(self, filename):
-
-        self.window.open_file(os.path.join(os.getcwd(), filename))
-
--- view.py
+++ view.py
@@ -4,30 +4,75 @@ import sublime_plugin
 
 
 def get_return(var_label):
+    """Get return value corresponding to a given label
+
+    Parameters
+    ----------
+    var_label : str
+        Key of return value to fetch from return_drop_file
+
+    Returns
+    -------
+    arbitrary type
+        Fetched return value
+    """
     ret = sublime.load_settings("return_drop_file.sublime-settings")
     return ret.get(var_label)
 
 
 class viewmanagerCommand(sublime_plugin.WindowCommand):
-    """
-    """
+    """View Manager command for creating, tracking, and destroying generated
+    views"""
 
     active_explorer_windows = {}
 
     def run(self, method, label):
-        """
+        """Command run method (as per sublime API)
+
+        Parameters
+        ----------
+        method : str
+            Method to call
+        label : str
+            Parameter to pass to the called method (usually the name of
+            the new view / file)
+
+        Returns
+        -------
+        All returns via get_return // return_drop_file.sublime-settings
+        view_id : int
+            View ID of the created / destroyed / found view
+        label : str
+            View name
         """
 
         ret = sublime.load_settings(
             'return_drop_file.sublime-settings')
 
-        (return_id, return_label) = getattr(self, method)(label)
-
-        ret.set("view_id", return_id)
-        ret.set("label", return_label)
+        try:
+            (return_id, return_label) = getattr(self, method)(label)
+            ret.set("view_id", return_id)
+            ret.set("label", return_label)
+        except AttributeError:
+            print(
+                "Method name not valid. Valid method names: "
+                "create_new_view\n"
+                "is_registered_view\n"
+                "close_if_registered\n"
+                "close_view")
 
     def create_new_view(self, label):
-        """
+        """Create a new view with the provided label
+
+        Parameters
+        ----------
+        label : str
+            View label
+
+        Returns
+        -------
+        (int, str)
+            (Created View ID, View label (same as arg))
         """
 
         # Create file
@@ -41,10 +86,20 @@ class viewmanagerCommand(sublime_plugin.WindowCommand):
         self.active_explorer_windows.update(
             {view_id: label})
 
-        return((view_id, label))
+        return (view_id, label)
 
     def is_registered_view(self, label):
-        """
+        """Check if a given label corresponds to a registered view
+
+        Parameters
+        ----------
+        label : str
+            View name to check
+
+        Returns
+        -------
+        (int, str)
+            (view_id, view_label) if found; else (0, "")
         """
 
         # get current view ID
@@ -57,6 +112,18 @@ class viewmanagerCommand(sublime_plugin.WindowCommand):
             return((0, ""))
 
     def close_if_registered(self, label):
+        """Check if a label is registered, and close it if it is.
+
+        Parameters
+        ----------
+        label : str
+            Ignored
+
+        Returns
+        -------
+        (int, str)
+            Same as is_registered_view.
+        """
 
         if self.is_registered_view(""):
             return self.close_view("")
@@ -64,7 +131,17 @@ class viewmanagerCommand(sublime_plugin.WindowCommand):
             return ((0, ""))
 
     def close_view(self, label):
-        """
+        """Close the target view
+
+        Parameters
+        ----------
+        label : str
+            Ignored
+
+        Returns
+        -------
+        (int, str)
+            Same as is_registered_view.
         """
 
         # get current view
